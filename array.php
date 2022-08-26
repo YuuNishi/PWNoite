00fc0766a4505d8e6785d9ba19403a8515439603
Nishi <84825027+YuuNishi@users.noreply.github.com>
2022-08-26 19:35:21 -0300

    Create array.php

---
+++ array.php
@@ -0,0 +1,20 @@
+<?php
+    $cars = array();
+    $cars = array("BMW","Saveiro","Gol", "Palio");
+    echo $cars[0];
+    echo $cars[1];
+    echo $cars[2];
+    echo $cars[3];
+
+    echo "<br>";
+
+    echo "<pre>";
+    print_r($cars);
+    echo "</pre>";
+
+    $names = ["Yuu", "Yuuya","Kei"];
+
+    echo "<pre>";
+    print_r($names);
+    echo "</pre>";
+?>
