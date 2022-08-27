1b23cb76fe92cf93dd2fa79bc58c622dc5ac724d
Nishi <84825027+YuuNishi@users.noreply.github.com>
2022-08-26 21:27:09 -0300

    Create sum e sub functions

--- funcao.php
+++ funcao.php
@@ -5,7 +5,14 @@
     function PrintText($text){
         echo $text;
     }
-
+    function Sum($number1, $number2){
+        $sum = $number1 + $number2;
+        PrintText($sum);
+    }
+    function Sub($number1, $number2){
+        $sub = $number1 - $number2;
+        PrintText($sub);
+    }
     echo "Yumi";
     PrintBR();
     echo "ADS";
